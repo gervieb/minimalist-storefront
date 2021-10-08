fb03a21baaaf1cdcfca80a0f90eebb0019667ef3
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-10-08 16:07:27 +0200

    split productlist component

--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,28 +1,13 @@
 import React, { Component } from "react";
-import whiteCart from "../assets/white-cart.png";
-import { addToCart } from "../redux/actions/cartAction";
-import { connect } from "react-redux";
-import { queryCategory, endpoint } from "../utils/query";
+
+import { quertProductByCategory, endpoint } from "../utils/query";
 import { request } from "graphql-request";
-import { increaseQty } from "../redux/actions/cartAction";
-import { handleAddToCart, currencyConverter } from "../utils/productUtils";
 import {
   CategoryName,
   Container,
   ProductListWrapper,
-  WhiteCartWrapper,
-  WhiteCart,
-  CartImage,
-  ImageWrapper,
-  Image,
-  ProductWrapper,
-  StyledLink,
-  OutOfStockWrapper,
-  OutOfStockText,
-  ProductName,
-  ProductPrice,
-  ProductLabel,
 } from "./ProductList.style";
+import ProductListDisplay from "../components/ProductListDisplay";
 
 class ProductList extends Component {
   state = {
@@ -30,20 +15,19 @@ class ProductList extends Component {
   };
 
   componentDidMount = () => {
-    this.queryProduct();
+    this.queryProduct().catch((error) => console.error(error));
   };
 
   queryProduct = async () => {
     const variables = {
       title: this.props.match.params.categoryName,
     };
-    const data = await request(endpoint, queryCategory, variables);
+    const data = await request(endpoint, quertProductByCategory, variables);
     this.setState({ productList: data.category.products });
   };
 
   render() {
-    const { match, cartList, currentCurr, increase, addItemToCart } =
-      this.props;
+    const { match, currentCurr } = this.props;
     const { productList } = this.state;
 
     const products =
@@ -54,71 +38,17 @@ class ProductList extends Component {
       <Container>
         <CategoryName>{match.params.categoryName}</CategoryName>
         <ProductListWrapper>
-          {products.map((product) => {
-            return (
-              <ProductWrapper key={product.id}>
-                <StyledLink
-                  to={{
-                    pathname: `${match.url}/${product.id}`,
-                    state: product,
-                  }}
-                >
-                  <ImageWrapper>
-                    <Image
-                      src={product.gallery[0]}
-                      alt={product.name}
-                      inStock={product.inStock}
-                    />
-                    {!product.inStock && (
-                      <OutOfStockWrapper>
-                        <OutOfStockText>OUT OF STOCK</OutOfStockText>
-                      </OutOfStockWrapper>
-                    )}
-                  </ImageWrapper>
-                </StyledLink>
-                {product.inStock && (
-                  <WhiteCartWrapper
-                    onClick={() =>
-                      handleAddToCart(
-                        product.prices,
-                        product,
-                        null,
-                        cartList,
-                        currentCurr,
-                        addItemToCart,
-                        increase
-                      )
-                    }
-                  >
-                    <WhiteCart>
-                      <CartImage src={whiteCart} alt="cart icon" />
-                    </WhiteCart>
-                  </WhiteCartWrapper>
-                )}
-                <ProductLabel inStock={product.inStock}>
-                  <ProductName>{product.name}</ProductName>
-                  <ProductPrice>
-                    {currentCurr}{" "}
-                    {currencyConverter(product.prices, currentCurr)}
-                  </ProductPrice>
-                </ProductLabel>
-              </ProductWrapper>
-            );
-          })}
+          {products.map((product) => (
+            <ProductListDisplay
+              key={product.id}
+              product={product}
+              currentCurr={currentCurr}
+            />
+          ))}
         </ProductListWrapper>
       </Container>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  currentCurr: state.currencyList.currency,
-  cartList: state.cart.cartList,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  addItemToCart: (item) => dispatch(addToCart(item)),
-  increase: (id) => dispatch(increaseQty(id)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
+export default ProductList;
