a260982e2f78c107e8142ac7109d75b02689fc98
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-10-08 16:04:37 +0200

    added display currency and display currency action

--- src/redux/actions/displayAction.js
+++ src/redux/actions/displayAction.js
@@ -1,8 +1,24 @@
 export const DISPLAY_CART = "DISPLAY_CART";
+export const DISPLAY_CURRENCY = "DISPLAY_CURRENCY";
+export const DISPLAY_SIDEPANEL = "DISPLAY_SIDEPANEL";
 
 export const displayCart = (bool) => {
   return {
     type: DISPLAY_CART,
+    payload: bool,
+  };
+};
+
+export const displayCurrency = (bool) => {
+  return {
+    type: DISPLAY_CURRENCY,
+    payload: bool,
+  };
+};
+
+export const displaySidepanel = (bool) => {
+  return {
+    type: DISPLAY_SIDEPANEL,
     payload: bool,
   };
 };
