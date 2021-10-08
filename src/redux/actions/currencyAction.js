5b52e56b01ef52454171379222eb5eb52a3b50ba
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-10-08 16:01:21 +0200

    added save currencies action

--- src/redux/actions/currencyAction.js
+++ src/redux/actions/currencyAction.js
@@ -1,8 +1,16 @@
 export const CHANGE_CURRENCY = "CHANGE_CURRENCY";
+export const SAVE_CURRENCIES = "SAVE_CURRENCIES";
 
 export const changeCurrency = (curr) => {
   return {
     type: CHANGE_CURRENCY,
+    payload: curr,
+  };
+};
+
+export const saveCurrencies = (curr) => {
+  return {
+    type: SAVE_CURRENCIES,
     payload: curr,
   };
 };
